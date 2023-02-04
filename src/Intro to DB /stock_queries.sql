42c0f9d55eb92c3f2fb54e42e47961cc5d9cd873
mina-ti <mina.ghebrial@trilogy.com>
2023-02-04 22:25:47 +0000

    fixed milestone 2 and 3

--- src/Intro to DB /stock_queries.sql
+++ src/Intro to DB /stock_queries.sql
@@ -1,7 +1,8 @@
 -- 1. Here I have created a query that returns the total_volume of a traded stock "Facebook" on a given date "02/02/2023" 
 SELECT SUM(volume) as total_volume
-FROM stocks
-WHERE stock_name = "Facebook" AND date="20230202"
+FROM dbAssMilestone2_OHLC
+WHERE market_id = (SELECT id FROM market_day WHERE date = 'YYYY-MM-DD')
+  AND stocks_id = (SELECT id FROM dbAssMilestone2_stocks WHERE name = 'stock_name');
 
 -- 2. This SQL query takes a persons sell trades as a profit and buy trades as a loss and sums the total to give the net profit_loss on a given date
 
@@ -12,39 +13,31 @@ GROUP BY date = "date"
 --3. This is an SQL query that takes stock_id, stock price and date from a stocks table and calculates the average price of each stock at the end of a month
 -- it then uses that information to calculate the percentage growth of that stock and returns a limit of 5 stocks
 
-WITH monthly_stock AS(
-SELECT
-    stock_id, date_trunc("month",'2023-01') AS current_month, date_trunc("month",'2022-12') AS previous_month, avg(stock_price) as price
-From stocks
-GROUP BY stock_id, month
-)
-
-SELECT
-    stock_id, (current_month-previous_month OVER (PARTITION BY stock_id ORDER BY current_month))/(previous_month OVER (PARTITION BY stock_id ORDER BY current_month))*100 
-    AS growth_percentage
-FROM monthly_stock
-ORDER BY growth_percentage
+SELECT stocks.name, (price - (LAG(price) OVER (PARTITION BY stocks.name ORDER BY date DESC))) / LAG(price) OVER (PARTITION BY stock_name ORDER BY date DESC) * 100 as percentage_growth
+FROM dbAssMilestone2_stock
+WHERE date >= DATEADD(month, -1, CURRENT_TIMESTAMP)
+GROUP BY stock_name, date, price
+ORDER BY percentage_growth DESC
 LIMIT 5
 
 --4. This is an SQL query that calcualtes the average buying prices for given stock
 
-SELECT user_id,stock_id,AVG(buy_price) as avg_buy_price
-FROM holdings
-WHERE stock_id="123"
+SELECT user_id, AVG(price) AS avg_price
+FROM dbAssMilestone2_holdings
+GROUP BY user_id;
 
 --5. This is an SQL query that first has a query that gets the current price of a stock on todays date and then calculates you profit or loss 
 -- by multiplying the number of shares a user has in the portfolio
 
-WITH current_prices AS (
-
-    SELECT stock_id, MAX(date) as today, AVG(close) as stock_price
-    FROM stocks
-    GROUP BY stock_id
-)
-
-SELECT
-    holdings.user_id
-    holdings.stock_id, (current_prices.stock_price- holdings.buy_price) * holdings.shares AS profit_loss
-    FROM holdings
-    JOIN stock_price
-    ON holdings.stock_id = current_prices.stock_id AND holdings.purchase_date = current_prices.today
+SELECT 
+  users.name,
+  SUM(CASE 
+    WHEN stocks.price > holdings.bid_price THEN (stocks.price - holdings.bid_price) * holdings.quantity
+    ELSE (holdings.bid_price - stocks.price) * holdings.quantity
+  END) AS net_profit_loss
+FROM 
+  holdings
+  JOIN stocks ON holdings.stocks_id = stocks.id
+  JOIN users ON holdings.user_id = users.id
+GROUP BY 
+  users.name;
--- src/Intro to DB /stock_schema.sql
+++ src/Intro to DB /stock_schema.sql
@@ -1,30 +1,74 @@
 CREATE TABLE `dbAssMilestone2_holdings` (
-  `id` int(11) NOT NULL,
-  `stock_id` int(11) DEFAULT NULL,
-  `volume` int(11) DEFAULT NULL,
-  `purchase_price` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` INT NOT NULL,
+  `volume` INT NULL,
+  `bid_price` DOUBLE NULL,
+  `bought_on` DATE NULL,
+  `users_id` INT NOT NULL,
+  `stocks_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `users_id`, `stocks_id`),
+  INDEX `fk_holdings_users1_idx` (`users_id` ASC) VISIBLE,
+  INDEX `fk_holdings_stocks1_idx` (`stocks_id` ASC) VISIBLE,
+  CONSTRAINT `fk_holdings_users1`
+    FOREIGN KEY (`users_id`)
+    REFERENCES `mydb`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_holdings_stocks1`
+    FOREIGN KEY (`stocks_id`)
+    REFERENCES `mydb`.`stocks` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
 
 CREATE TABLE `dbAssMilestone2_OHLC` (
-  `id` int(11) NOT NULL,
-  `open` int(11) DEFAULT NULL,
-  `close` int(11) DEFAULT NULL,
-  `high` int(11) DEFAULT NULL,
-  `low` int(11) DEFAULT NULL,
-  `volume` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `stocks_id` INT NOT NULL,
+  `market_id` INT NOT NULL,
+  `open` DOUBLE NULL,
+  `high` DOUBLE NULL,
+  `low` DOUBLE NULL,
+  `close` DOUBLE NULL,
+  `volume` INT NULL,
+  PRIMARY KEY (`stocks_id`, `market_id`),
+  INDEX `fk_stocks_has_market_day_market_day1_idx` (`market_id` ASC) VISIBLE,
+  INDEX `fk_stocks_has_market_day_stocks_idx` (`stocks_id` ASC) VISIBLE,
+  CONSTRAINT `fk_stocks_has_market_day_stocks`
+    FOREIGN KEY (`stocks_id`)
+    REFERENCES `mydb`.`stocks` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_stocks_has_market_day_market_day1`
+    FOREIGN KEY (`market_id`)
+    REFERENCES `mydb`.`market_day` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 CREATE TABLE `dbAssMilestone2_orders` (
-  `id` int(11) NOT NULL,
-  `stock_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `purchase_price` int(11) DEFAULT NULL,
-  `volume` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` INT NOT NULL,
+  `bid_price` DOUBLE NULL,
+  `type` VARCHAR(4) NULL,
+  `created_at` TIMESTAMP NULL,
+  `updated_at` TIMESTAMP NULL,
+  `status` VARCHAR(20) NULL,
+  `bid_volume` INT NULL,
+  `executed_volume` INT NULL,
+  `users_id` INT NOT NULL,
+  `stocks_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `users_id`, `stocks_id`),
+  INDEX `fk_orders_users1_idx` (`users_id` ASC) VISIBLE,
+  INDEX `fk_orders_stocks1_idx` (`stocks_id` ASC) VISIBLE,
+  CONSTRAINT `fk_orders_users1`
+    FOREIGN KEY (`users_id`)
+    REFERENCES `mydb`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_orders_stocks1`
+    FOREIGN KEY (`stocks_id`)
+    REFERENCES `mydb`.`stocks` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 CREATE TABLE `dbAssMilestone2_sectors` (
   `id` int(11) NOT NULL,
@@ -33,18 +77,35 @@ CREATE TABLE `dbAssMilestone2_sectors` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `dbAssMilestone2_stock` (
-  `id` int(11) NOT NULL,
-  `stock_name` varchar(45) DEFAULT NULL,
-  `sector_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` INT NOT NULL,
+  `name` VARCHAR(20) NULL,
+  `total_volume` INT NULL,
+  `unallocated` INT NULL,
+  `price` DOUBLE NULL,
+  `sectors_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `sectors_id`),
+  INDEX `fk_stocks_sectors1_idx` (`sectors_id` ASC) VISIBLE,
+  CONSTRAINT `fk_stocks_sectors1`
+    FOREIGN KEY (`sectors_id`)
+    REFERENCES `mydb`.`sectors` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 CREATE TABLE `dbAssMilestone2_users` (
-  `user_id` int(11) NOT NULL,
-  `Full Name` varchar(45) DEFAULT NULL,
-  `Country of Residence` varchar(45) DEFAULT NULL,
-  `stock_id` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` INT NOT NULL,
+  `name` VARCHAR(50) NULL,
+  `email` VARCHAR(50) NULL,
+  `available_funds` DOUBLE NULL,
+  `blocked_funds` DOUBLE NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
+
+CREATE TABLE IF NOT EXISTS `dbAssMilestone2_market_day` (
+  `id` INT NOT NULL,
+  `day` INT NULL,
+  `status` VARCHAR(10) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
