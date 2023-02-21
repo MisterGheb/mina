d7bc7613e8316ad3c06a4ef554e4de3248e6da18
mina-ti <mina.ghebrial@trilogy.com>
2023-02-21 21:43:27 +0000

    updated Murder mystery

--- SQL Murder Mystery/murder_myster.sql
+++ SQL Murder Mystery/murder_myster.sql
@@ -1,10 +1,51 @@
 
 
 
-SELECT * FROM get_fit_now_member WHERE person_id=(SELECT person_id FROM interview WHERE person_id=(SELECT id FROM person
-WHERE ((SELECT description FROM crime_scene_report
-	WHERE date=20180115 AND city='SQL City' AND type='murder') 
-	LIKE '%'||address_street_name||'%' AND name LIKE '%Annabel Miller%')
-UNION ALL
-SELECT id FROM person
-WHERE (address_street_name='Northwestern Dr') AND address_number='4919'))
+-- THE MURDERER:
+WITH gym_checkins AS (
+    SELECT person_id, name
+    FROM get_fit_now_member
+    JOIN get_fit_now_check_in ON get_fit_now_member.id = get_fit_now_check_in.membership_id
+    WHERE membership_status = 'gold'
+      AND id LIKE '48Z%'
+      AND check_in_date = '20180109'
+), suspects AS (
+    SELECT gym_checkins.person_id, gym_checkins.name, plate_number, gender
+    FROM gym_checkins
+    JOIN person ON gym_checkins.person_id = person.id
+    JOIN drivers_license ON person.license_id = drivers_license.id
+)
+SELECT name FROM suspects
+WHERE INSTR(plate_number, 'H42W') > 0 AND gender = 'male';
+
+-- name:
+-- Jeremy Bowers
+
+-- THE GUY BEHIND THE SCENES:
+
+WITH event_goers AS (
+    SELECT person_id, COUNT(1) AS n_checkins
+    FROM facebook_event_checkin
+    WHERE event_name = 'SQL Symphony Concert'
+      AND date LIKE '201712%'
+    GROUP BY person_id
+    HAVING n_checkins = 3
+),
+matching_suspects AS (
+    SELECT id AS license_id
+    FROM drivers_license
+    WHERE gender = 'female' AND hair_color = 'red'
+      AND car_make = 'Tesla' AND car_model = 'Model S'
+      AND height >= 64 AND height <= 68
+), 
+suspects AS (
+    SELECT person.id AS person_id, name, annual_income
+    FROM matching_suspects AS ms
+    JOIN person ON ms.license_id = person.license_id
+    JOIN income ON person.ssn = income.ssn
+)
+SELECT name
+FROM suspects
+JOIN event_goers ON suspects.person_id = event_goers.person_id;
+
+-- Miranda Priestly
